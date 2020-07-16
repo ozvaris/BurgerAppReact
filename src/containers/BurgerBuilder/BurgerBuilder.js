bb589f725c2b4edc30cef76675f2130fbbc30463
ozvaris <ozgurozvaris@hotmail.com>
2020-07-16 11:31:08 +0300

    Order summary component added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,7 +3,8 @@ import React, {Component } from 'react';
 import Aux from '../../hoc/Auxliary'
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
-
+import Modal from '../../components/UI/Modal/Modal'
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
 import { render } from '@testing-library/react'
 
 
@@ -83,6 +84,9 @@ class BurgerBuilder extends Component {
         }
         return (
             <Aux>
+                <Modal > 
+                    <OrderSummary ingredients={this.state.ingredients} />
+                </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls 
                     ingredientAdded={this.addIngredientHandler} 
