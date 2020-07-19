92f0cd5e2d1346c0e31ae97ade81968a09cead27
ozvaris <ozgurozvaris@hotmail.com>
2020-07-19 15:46:39 +0300

    price and cancel contine buttone added

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Aux from '../../../hoc/Auxliary'
+import Button from '../../UI/Button/Button'
 
 const orderSummary = (props) => {
 
@@ -20,7 +21,11 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
+            <p><strong>Total Price: {props.price}</strong></p>
             <p>Continue to Checkout?</p>
+            <Button clicked={props.purchaseCancelled} btnType= {"Danger"}>CANCEL</Button>
+            <Button clicked={props.purchaseContinued} btnType= {"Success"}>CONTINUE</Button>
+
         </Aux>
 
     );
