bbd776401b5bee1f8f9a1074a9e1f11e783cbe8a
ozvaris <ozgurozvaris@hotmail.com>
2020-07-29 16:32:52 +0300

    Firebase axios post data added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,25 +4,25 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
-    const transformedIngredients0 = Object.keys(props.ingredients)
-    .map(igKey => {
-        return[...Array(3)]
+    // const transformedIngredients0 = Object.keys(props.ingredients)
+    // .map(igKey => {
+    //     return[...Array(3)]
 
-    });
+    // });
 
-    const transformedIngredients1 = Object.keys(props.ingredients)
-    .map(igKey => {
-        return[...Array(props.ingredients[igKey])];
+    // const transformedIngredients1 = Object.keys(props.ingredients)
+    // .map(igKey => {
+    //     return[...Array(props.ingredients[igKey])];
 
-    });
+    // });
 
-    const transformedIngredients2 = Object.keys(props.ingredients)
-    .map(igKey => {
-        return[...Array(props.ingredients[igKey])].map((_, i) =>{
-           return <div  key={igKey +1} type={igKey} />;
-        });
+    // const transformedIngredients2 = Object.keys(props.ingredients)
+    // .map(igKey => {
+    //     return[...Array(props.ingredients[igKey])].map((_, i) =>{
+    //        return <div  key={igKey +1} type={igKey} />;
+    //     });
 
-    });
+    // });
 
     let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
@@ -40,12 +40,12 @@ const burger = (props) => {
         transformedIngredients = <p>Please start adding ingredients!</p>
     }
 
-    console.log(transformedIngredients0);
-    console.log(props.ingredients);
-    console.log(Object.keys(props.ingredients));
-    console.log(transformedIngredients1);
-    console.log(transformedIngredients2);
-    console.log(transformedIngredients);
+    //console.log(transformedIngredients0);
+    //console.log(props.ingredients);
+    //console.log(Object.keys(props.ingredients));
+    //console.log(transformedIngredients1);
+    //console.log(transformedIngredients2);
+    //console.log(transformedIngredients);
 
     return (
         <div className={classes.Burger}>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import axios from '../../axios-orders';
 import { render } from '@testing-library/react'
 
 
@@ -91,6 +92,24 @@ class BurgerBuilder extends Component {
     purchaseContinueHandler = () => {
         
        alert('You continue!');
+       const order ={
+           ingredients: this.state.ingredients,
+           price: this.state.totalPrice,
+           customer:{
+               name: 'Max Schwarzmüller',
+               address: {
+                   street: 'Teststreet 1',
+                   zipCode: '41351',
+                   country: 'Germany'
+               },
+               email: 'test@test.com'
+           },
+           deliveryMethod: 'fasttest'
+       }
+
+       axios.post('orders.json', order)
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
         
     }
 
