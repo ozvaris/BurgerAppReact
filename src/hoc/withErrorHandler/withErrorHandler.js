321c5bad82646bbccf7beda73890adebc92bb181
ozvaris <ozgurozvaris@hotmail.com>
2020-08-06 18:57:43 +0300

    Routing completed

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -25,16 +25,16 @@ const withErrorHandler = (WrappedComponent, axios) => {
             error: null
         }
 
-        componentWillMount() {
-            axios.interceptors.request.use(req => {
-                this.setState({ error: null});
-                return req;
-            });
+        // componentWillMount() {
+        //     axios.interceptors.request.use(req => {
+        //         this.setState({ error: null});
+        //         return req;
+        //     });
 
-            axios.interceptors.response.use(res => res, error => {
-                this.setState({error: error});
-            });
-        }
+        //     axios.interceptors.response.use(res => res, error => {
+        //         this.setState({error: error});
+        //     });
+        // }
 
         errorConfirmedHandler = () => {
             this.setState({error: null})
