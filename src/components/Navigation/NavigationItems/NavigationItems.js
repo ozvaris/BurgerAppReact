70260e94d9d40db75b5a9ec358082191a399a117
ozvaris <ozgurozvaris@hotmail.com>
2020-09-17 17:30:21 +0300

    logout added

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -10,7 +10,10 @@ const navigationItems = (props) => (
    <ul className={classes.NavigationItems}>
       <NavigationItem link="/" exact>Burger Builder</NavigationItem >
       <NavigationItem link="/orders">Orders</NavigationItem >
-      <NavigationItem link="/auth">Login</NavigationItem >
+      {!props.isAuthenticated
+      ?<NavigationItem link="/auth">Login</NavigationItem >
+      :<NavigationItem link="/logout">Logout</NavigationItem >}
+      
 
 
    </ul>
