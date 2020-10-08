0bce4d061fa6b2dd34f003b60c2ac531b6533552
ozvaris <ozgurozvaris@hotmail.com>
2020-10-08 13:54:35 +0300

    route guard added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 
 import { connect } from 'react-redux';
 import * as actions from './store/actions/index';
@@ -20,15 +20,28 @@ class App extends Component {
 
 
   render () {
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
+      </Switch>
+    );
+
+    if(this.props.isAuthenticated){
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to="/" />
+        </Switch>
+    }
+
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/" exact component={BurgerBuilder} />
-          </Switch>
+          {routes}
         </Layout>
       </div>
     );
@@ -37,9 +50,8 @@ class App extends Component {
 
 const mapStateToProps = state => {
   return {
-      orders: state.order.orders,
-      loading: state.order.loading,
-      token: state.auth.token
+      isAuthenticated: state.auth.token !== null,
+      
 
   }
 }
